e25852b37dddb29f7663093df72a41fc25385a75
Tanvir Rudro <rudro987@gmail.com>
2024-05-16 22:35:50 +0600

    Error handling in express

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -34,21 +43,39 @@ const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
-app.get("/", logger, (req, res) => {
-    res.send("Hello from tanvir server!!");
+app.get("/", logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(req.query);
+        res.send("Hello from Tanvir's server");
+    }
+    catch (error) {
+        console.log(error);
+        next(error);
+    }
+}));
+app.get('/:userId', (req, res) => {
+    console.log(req.params);
+    res.send("Hello from tanvir's server!!");
 });
-// app.get('/:userId', (req: Request, res: Response) => {
-//     console.log(req.params);
-//     res.send("Hello from tanvir's server!!");
-// });
-// app.get('/', (req: Request, res: Response) => {
-//     console.log(req.query);
-//     res.send("Hello from tanvir's server!!");
-// });
 app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "Successfully received data",
     });
+});
+app.all("*", (req, res) => {
+    res.status(400).json({
+        success: false,
+        message: "Route is not found",
+    });
+});
+// global error handler
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: 'Failed to get data',
+        });
+    }
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 
 const app = express();
 
@@ -39,19 +39,21 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-app.get("/", logger, (req: Request, res: Response) => {
-  res.send("Hello from tanvir server!!");
+app.get("/", logger, async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(req.query);
+    res.send("Hello from Tanvir's server");
+  } catch (error) {
+    console.log(error);
+    next(error)
+  }
 });
 
-// app.get('/:userId', (req: Request, res: Response) => {
-//     console.log(req.params);
-//     res.send("Hello from tanvir's server!!");
-// });
+app.get('/:userId', (req: Request, res: Response) => {
+    console.log(req.params);
+    res.send("Hello from tanvir's server!!");
+});
 
-// app.get('/', (req: Request, res: Response) => {
-//     console.log(req.query);
-//     res.send("Hello from tanvir's server!!");
-// });
 
 app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
@@ -59,5 +61,25 @@ app.post("/", logger, (req: Request, res: Response) => {
     message: "Successfully received data",
   });
 });
+
+
+app.all("*", (req: Request, res: Response) => {
+    res.status(400).json({
+        success: false,
+        message: "Route is not found",
+    })
+})
+
+// global error handler
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if(error){
+        res.status(400).json({
+        success: false,
+        message: 'Failed to get data',
+    })
+    }
+    
+})
 
 export default app;
